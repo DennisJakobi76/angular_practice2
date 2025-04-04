3f88c8db3f540df5750cb6dd417bb12de1e4ab99
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-04 11:49:12 +0200

    added results to table

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestmenmtInput {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+}
+
+// export type InvestmentInputs = {
+// 	initialInvestment: number;
+// 	annualInvestment: number;
+// 	expectedReturn: number;
+// 	duration: number;
+// }
